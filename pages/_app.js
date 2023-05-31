04b292a8ec1c37b09957ade91dde6ef64584aa9b
Foxona <sh0n.mishel34@gmail.com>
2023-05-31 14:27:46 +0200

    fa-aswesome package installed

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles.css'
+import 'font-awesome/css/font-awesome.min.css';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
--- pages/_document.js
+++ pages/_document.js
@@ -9,10 +9,6 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap&subset=cyrillic" />
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"
-          />
         </Head>
         <body>
           <Main />
